76ebe93b7901aad5703b1d164772468d0f4ed383
Andres Bolivar <andresmaster1@gmail.com>
2023-02-07 11:09:10 +0400

    Updating file

--- vsix-downloader.sh
+++ vsix-downloader.sh
@@ -4,8 +4,8 @@ read -p "Item name: " ITEM_NAME
 read -p "Version: " VERSION
 PUBLISHER=$(echo $ITEM_NAME | cut -d '.' -f 1)
 NAME=$(echo $ITEM_NAME | cut -d '.' -f 2)
-echo -e "\n========== Creating URL =========="
+echo -e "\n========== Creating URL ==========\n"
 URL=https://$PUBLISHER.gallery.vsassets.io/_apis/public/gallery/publisher/$PUBLISHER/extension/$NAME/$VERSION/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage
 echo 'URL for downloading: ' $URL
-echo -e "\n========== Downloading with name $NAME.vsix =========="
+echo -e "\n========== Downloading $NAME.vsix version $VERSION ==========\n"
 curl -o $NAME.vsix $URL
